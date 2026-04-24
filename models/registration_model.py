39d2aa4e6e987052d4e5ae42bff7f59e4b891c3f
AreSh-a <179856116+AreSh-a@users.noreply.github.com>
2026-04-25 00:57:53 +0500

    updated registration model

--- models/registration_model.py
+++ models/registration_model.py
@@ -10,11 +10,24 @@ class Registration(db.Model):
 
 class Payment(db.Model):
     __tablename__ = 'PAYMENTS'
- 
+    paymentid = db.Column('PAYMENTID', db.Integer, primary_key=True)
+    paymentstatus = db.Column('PAYMENTSTATUS', db.String(20))
+    eventid = db.Column('EVENTID', db.Integer, db.ForeignKey('EVENTS.EVENTID'))
+    amount = db.Column('PAYMENTSTATUS', db.Decimal(10, 2))
+    userid = db.Column('USERID', db.Integer, db.ForeignKey('USERS.USERID'))
  
 class Feedback(db.Model):
     __tablename__ = 'FEEDBACK'
-
+    feedbackid = db.Column('FEEDBACK', db.Integer, primary_key=True)
+    userid = db.Column('USERID', db.Integer, db.ForeignKey('USERS.USERID'))
+    eventid = db.Column('EVENTID', db.Integer, db.ForeignKey('EVENTS.EVENTID'))
+    rating = db.Column('FEEDBACK', db.Integer, db.CheckConstraint("rating > 0 and rating < 6"))
+    feedbacktext = db.Column('FEEDBACK', db.String(20))
 
 class AdminLog(db.Model):
     __tablename__ = 'ADMIN_LOG'
+    logid = db.Column('ADMIN_LOG', db.Integer, primary_key=True)
+    userid = db.Column('USERID', db.Integer, db.ForeignKey('USERS.USERID'))
+    actiontime = db.Column('ADMIN_LOG', db.DateTime, default=datetime.utcnow)
+    action = db.Column('ADMIN_LOG', db.String(20))
+    
