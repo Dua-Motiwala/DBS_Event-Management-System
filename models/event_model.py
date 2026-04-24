460decbe2732daf7b509807eef9e3c3fae7d0960
Ateeba Noor <ateebanoor056@gmail.com>
2026-04-24 22:20:35 +0500

    event_model updated and relationships added

--- models/event_model.py
+++ models/event_model.py
@@ -3,18 +3,26 @@ from datetime import datetime
 
 class Venue(db.Model):
     __tablename__ = 'VENUES'
-
+    venueid = db.Column('VENUEID', db.Integer, primary_key=True)
+    name = db.Column('NAME', db.String(100))
+    location = db.Column('LOCATION', db.String(100))
+    capacity = db.Column('CAPACITY', db.Integer)
     events = db.relationship('Event', backref='venue', lazy=True)
 
 class Category(db.Model):
     __tablename__ = 'CATEGORIES'
-
+    categoryid = db.Column('CATEGORYID', db.Integer, primary_key=True)
+    categoryname = db.Column('CATEGORYNAME', db.String(100))
     events = db.relationship('Event', backref='category', lazy=True)
-
+    
 class Event(db.Model):
     __tablename__ = 'EVENTS'
-
-
+    eventid = db.Column('EVENTID', db.Integer, primary_key=True)
+    title = db.Column('TITLE', db.String(100))
+    userid = db.Column('USERID', db.Integer, db.ForeignKey('USERS.USERID'))
+    eventdate = db.Column('EVENTDATE', db.Date)
+    venueid = db.Column('VENUEID', db.Integer, db.ForeignKey('VENUES.VENUEID'))
+    categoryid = db.Column('CATEGORYID', db.Integer, db.ForeignKey('CATEGORIES.CATEGORYID'))
     registrations = db.relationship('Registration', backref='event', lazy=True)
     payments = db.relationship('Payment', backref='event', lazy=True)
     feedbacks = db.relationship('Feedback', backref='event', lazy=True)
@@ -22,4 +30,8 @@ class Event(db.Model):
 
 class EventSchedule(db.Model):
     __tablename__ = 'EVENT_SCHEDULE'
-
+    scheduleid = db.Column('SCHEDULEID', db.Integer, primary_key=True)
+    eventid = db.Column('EVENTID', db.Integer, db.ForeignKey('EVENTS.EVENTID'))
+    start_time = db.Column('START_TIME', db.DateTime)   
+    end_time = db.Column('END_TIME', db.DateTime)
+    
